268fe3d1d1baa3edbb0aefd29e8687c95640e067
Narione <ynhp3214@gmail.com>
2024-05-22 19:44:50 +0900

    240522 eGovWeb first commit

---
+++ src/main/java/egovframework/example/login/web/LoginController.java
@@ -0,0 +1,86 @@
+package egovframework.example.login.web;
+
+import egovframework.example.exception.MemberNotFoundException;
+import egovframework.example.login.service.LoginRequest;
+import egovframework.example.login.service.LoginService;
+import egovframework.example.member.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class LoginController {
+    private final LoginService service;
+
+    @GetMapping("/login")
+    public String loginView(String location, Model model) {
+        model.addAttribute("location", location);
+        return "common/login";
+    }
+
+    @PostMapping("/login")
+    public String login(LoginRequest login, HttpSession session, Model model) {
+        Member member = service.findMember(login);
+        if (member == null) {
+            throw new MemberNotFoundException();
+        }
+        session.setAttribute("member", member);
+        return "redirect:/";
+
+
+//        if(member != null) {
+//            session.setAttribute("member", member);
+//            return "redirect:/index";
+//        }
+//        model.addAttribute("msg", "로그인 실패");
+//        return "common/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        //세션의 모든 데이터 지우기
+        session.invalidate();
+        return "redirect:/index";
+    }
+
+    @PostMapping("/ajaxLogin")
+    @ResponseBody
+    public Map<String, String> ajaxLogin(LoginRequest login, @RequestParam(required = false) String location, HttpSession session) {
+
+        // 성공할 경우 {"msg" : "success"}
+        // 실패할 경우 {"msg" : "failed"}
+        log.debug("location: {}", location);
+        HashMap<String, String> map = new HashMap<>();
+        Member member = service.findMember(login);
+        if (member == null) {
+            throw new MemberNotFoundException();
+        }
+
+        session.setAttribute("member", member);
+        if (location != null && !"".equals(location)) {
+            map.put("msg", location);
+        } else {
+            map.put("msg", "/");
+        }
+
+        return map;
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    @ResponseBody
+    public Map<String, String> memberNotFound(Model model) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("msg", "failure");
+        return map;
+    }
+
+
+}
